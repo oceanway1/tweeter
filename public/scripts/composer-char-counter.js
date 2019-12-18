18df8738c407a8ed509195254a2763b4019c4a2d
oceanway1 <oceanway1@yahoo.ca>
2019-12-18 23:59:22 +0000

    Implement Character Count For Tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+// var $ = require("jquery");
+$(document).ready(function() {
+  // --- our code goes here ---
+  // console.log("Testing to see if loadedS");
+  $(".tweet-text").keyup(function() {
+    const maxCharacter = 140;
+    const limit = $(this).val().length;
+    let result = maxCharacter - limit;
+    const $count = $(".counter");
+    $count.text(result);
+    if (result <0) {
+      $count.addClass("red");
+    } else {
+      $count.removeClass("red");
+    }
+  });
+});
