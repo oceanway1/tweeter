2f821953b94f4d3b2d9fe41e8deffa75670e311f
oceanway1 <oceanway1@yahoo.ca>
2019-12-19 21:11:56 +0000

    Implement Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,8 +16,47 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
-const $tweet = createTweetElement(tweetData);
+//This function takes in an array of objects, reverses its order and then transform each element 
+// in the array (which should be objects) into html text;
+  const createTweetElement = function(tweetData) {
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet);
+  // let  $tweet = $("<article>").addClass("tweet");
+  
+  // create header in tweeter container
+  const newDateStr = createDateString(tweetData.created_at);
+  const $spanCHImage = $('<span>').addClass("h-image");
+  const $img = $("<img>").attr("src", tweetData.user.avatars);
+  $spanCHImage.append($img);
+  const $spanHName = $('<span>').addClass("h-name");
+  const $h3Name = $("<h3>").text(tweetData.user.name);
+  $spanHName.append($h3Name);
+  const $spanHaName = $("<span>").addClass("h-aname");
+  const $h3aName = $("<h3>").text(tweetData.user.handle);
+  $spanHaName.append($h3aName);
+  const $header = $('<header>')
+  $header.append($spanCHImage, $spanHName, $spanHaName);
+  
+  //create tweet message body in tweeter container
+  const $pTweet = $("<p>").text(tweetData.content.text);
+    
+  //creat footer in tweeter container
+  const $spanFLeft = $("<span>").addClass("footer-left");
+  $spanFLeft.append($("<h4>").text(newDateStr));
+  const $spanFRight = $("<span>").addClass("footer-right");
+  const $iFlag = $("<i>").addClass("fas fa-flag").attr("style");
+  const $iRetweet = $("<i>").addClass("fas fa-retweet").attr("style");
+  const $iHeart = $("<i>").addClass("fas fa-heart").attr("style");
+  $spanFRight.append($iFlag, $iRetweet, $iHeart);
+  const $footer = $("<footer>").addClass("foot");
+  $footer.append($spanFLeft, $spanFRight);
+
+  //append header, body, and footer to container
+
+  $tweet.append($header, $pTweet, $footer);
+  console.log($tweet);
+};
+
+ 
+
+
+
