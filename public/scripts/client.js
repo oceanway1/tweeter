cbfe28cd4e051f90d213a192dc98723e4e95b254
oceanway1 <oceanway1@yahoo.ca>
2019-12-21 05:27:55 +0000

    Implement eror elements, cross site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,99 +4,136 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+$(document).ready(function () {
 
-const createTweetElement = function (tweet) {
-  let date = new Date(tweet.created_at).toDateString();
-  const $tweet = `
-  <main <article class="tweet">
-    <header class="container-tweet">
-      <div class="container-tweet">
-       <img class="h-image" src=${tweet.user.avatars}/>
-       <span class="h-name">${tweet.user.name}</span>
-      </div>
-      <span class="h-aname">${tweet.user.handle}</span>
-    </header>
-  ${tweet.content.text}
-  <footer>
-    <div class = "tweet-age">${date}<div>
-      <span class="icon-span">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-      </span>
-    </div>
-  </footer>
-</article>
-</main>
-`;
-  return $tweet;
+
+  //This function escapes XSS(cross-site scripting)
+const escape = function (str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
 
 
-const renderTweets = function (tweets) {
-  for (let x of tweets) {
-    $tweet = createTweetElement(x)
-    $('#tweets-container').prepend($tweet)
+  const createTweetElement = function (tweet) {
+    let date = new Date(tweet.created_at).toDateString();
+    const $tweet = `
+  <article class="tweet">
+    <header>
+      <div>
+        <span>
+          <img class="h-image" src="${tweet.user.avatars}">
+        </span>
+        <span class="h-name">
+          <h3>${tweet.user.name}</h3>
+        </span>
+        <span class="h-aname">
+          <h3>${tweet.user.handle}</h3>
+        </span>
+
+      </div>
+    </header>
+    <p> ${escape(tweet.content.text)}</p>
+    <footer class="foot">
+      <span class="footer-left">
+        <h4>${date}</h4>
+      </span>
+      <span class="footer-right">
+        <i class='fa fa-flag'></i>
+        <i class='fa fa-retweet'></i>
+        <i class='fa fa-heart'></i>
+      </span>
+    </footer>
+  </article>
+
+`;
+    return $tweet;
+  };
+
+
+  // renderTweets(data)
+
+  const renderTweets = function (tweets) {
+    $(".tweet-container").empty();
+    for (let x of tweets) {
+      $tweet = createTweetElement(x)
+      $(".tweet-container").prepend($tweet)
+    }
   }
-}
-renderTweets(data);
+
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    })
+      .then(function (newTweet) {
+        renderTweets(newTweet);
+      });
+  };
+
+  loadTweets();
+
+  const $newTweet = $(".new-tweet");
+  let isNew = false;
+  $newTweet.hide();
+
+  $(".write-tweet").click(function () {
+    if (!isNew) {
+      $newTweet.slideToggle();
+      $("textarea").val('').focus();
+      $(".counter").text('140');
+      isNew = true;
+    }
+  })
 
 
+  // create error element
+  const $error = $(".error");
+  const $errMessage = $('.err-message');
+  $error.hide();
 
-$(document).ready(function () {
 
   $(".compose-tweet").submit(function (event) {
     event.preventDefault();
+    const tweetLength = $(".tweet-text").val().length;
     const newTweet = $(".tweet-text").val();
-
-    // $.ajax({
-    //   url: "/tweets",
-    //   method: "POST",
-    //   data: $('.compose-tweet').serialize(),
-    //   success: function () {
-    //     // $(index).html(data);
-    //     $('.tweet-container').empty(); 
-    //   }
-    // })
-    //   .done(function (data) {
-    //     console.log('done:', data);
-
-    //   })
-    // const formData = (this).serialize();
-
+    if (newTweet === "") {
+      $errMessage.text("Tweet can not be empty, please enter your Tweet below");
+      $error.slideDown();
+      return;
+    }
+    if (tweetLength > 140) {
+      $errMessage.text("Only 140 characters allowed.");
+      $error.slideDown();
+      return;
+    }
+    const result = { text: newTweet }
     $.ajax({
       url: "/tweets",
       method: "POST",
-      data: newTweet
+      data: result, 
+      success: function () {
+        $('.tweet-container').empty(); // clear the tweets
+        loadTweets(); // reloading the tweets 
+      }
     })
-      .then((function () {
-        console.log("Success")
-      }));
-
+      .done(function (textStatus) {
+        console.log('done:', textStatus);
+        $error.hide();
+        $('.new-tweet').hide();
+        $error.slideUp();
+        isNew = false;
+      });
   })
 })
+
+
+
+
+
+
+
+
+
+
+
